8462cc8118cf523fa7bca206e3d36d16b7790a0d
Rodthung Sudrung <r.sudrung@gmail.com>
2016-05-09 03:05:30 -0700

    First Commit

--- app/main.js
+++ app/main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 angular.module("newsApp", ["ngRoute"])
 
 .config(function ($routeProvider, $locationProvider) {
@@ -14,25 +13,4 @@ angular.module("newsApp", ["ngRoute"])
 
 
 .run(function () {
-
-
-=======
-angular.module("newsApp", ["ngRoute"])
-
-.config(function ($routeProvider, $locationProvider) {
-    $routeProvider
-        .when('/', {
-            templateUrl: 'pages/home.html'
-        })
-        .when('/Map', {
-            templateUrl: 'pages/map.html',
-            controller: 'mapController'
-        });
-})
-
-
-.run(function () {
-
-
->>>>>>> 28af370dab4d74096807671e9d45b621b9fc643b
 });
